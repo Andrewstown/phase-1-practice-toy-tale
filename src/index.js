12a9767f18640a50abbf96d0a1602cde7bac8e78
+ <stingeyandrewstown@live.com>
2023-01-16 17:40:21 -0600

    yes

--- src/index.js
+++ src/index.js
@@ -12,4 +12,55 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  grabToys()
 });
+
+document.querySelector('.add-toy-form').addEventListener('submit', handleToy)
+
+function renderToy(toy){
+  let card = document.createElement('li')
+  card.className = 'card'
+  card.innerHTML = `
+  <h2>${toy.name}</h2>
+  <img src ="${toy.image}" class="toy-avatar"/>
+  <p class="likes">${toy.likes} Likes</p>
+  <button class="like-btn" id="${toy.id}">Like ❤️</button>`
+
+  card.querySelector('.like-btn').addEventListener('click', () => {
+    toy.likes += 1
+    card.querySelector('.likes').textContent = `${toy.likes} Likes`
+    fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method:'PATCH',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toy)
+    })
+  })
+
+  document.querySelector("#toy-collection").appendChild(card)
+}
+
+function grabToys(){
+  fetch("http://localhost:3000/toys/")
+  .then (res => res.json())
+  .then (data => data.forEach(toy => renderToy(toy)))
+}
+
+function handleToy(e){
+  e.preventDefault()
+  let t = e.target
+  let toy = {
+    name: t.name.value,
+    image: t.image.value,
+    likes: 0
+  }
+  renderToy(toy)
+  fetch("http://localhost:3000/toys/", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(toy)
+  })
+}
